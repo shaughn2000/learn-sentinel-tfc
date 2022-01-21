33c76ca016fa72100129735709d8774d1908e6f1
shaughn2000 <shaughn2000@pobox.com>
2022-01-20 22:46:36 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "shaughn2000"
 
     workspaces {
       name = "sentinel-example"
